86065de0e45bf546aa9b8ff29e15a08f84e03b81
Ashish Singh <ashishsingh.1899@gmail.com>
2023-03-10 17:16:43 +0530

    1st stage

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+// Define possible choices for the game
+const choices = ["rock", "paper", "scissors"];
+
+// Generate a random choice for the computer player
+function computerPlay() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const toggleBtn = document.getElementById('btn');
 const toggleDiv = document.getElementById('rule');
 const cross = document.getElementById('cross');
